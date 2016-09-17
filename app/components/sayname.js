3dc7cd154d4c05ddce33328a86954ce2ab036563
George Kiknadze <g.kiknadze@hotmail.com>
2016-09-17 15:37:43 +0200

    Repair Import Export

---
+++ app/components/sayname.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class SayName extends React.Component {
+  render() {
+    return (
+       <input type="text" defaultValue="Hello!" />
+    );
+  }
+};
--- app/index.js
+++ app/index.js
@@ -1,21 +1,20 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-// const React = require('react');
-// const ReactDOM = require('react-dom');
+import SayName from './components/sayname.js';
 
 
-class HelloMessage extends React.Component {
+export default class App extends React.Component {
 	render () {
 		return (
 			<div>
-				<h1> Hello React </h1>
-				<h3> Made by {this.props.name} </h3>
+				<h1>React boilerplate</h1>
+				<SayName />
 			</div>
 		);
 	}
 }
 
 ReactDOM.render(
-	<HelloMessage name='George Kiknadze' />,
+	<App />,
 	document.getElementById('app')
 );
